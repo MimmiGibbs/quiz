6fb6a477117e741fb53f843c6fba5f94f1b0d3bc
MimmiGibbs <mimmisethfors@gmail.com>
2025-02-25 15:46:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,227 @@
+const quizes = [
+    {
+        "category": "movie",
+        "questions": [
+            {
+                "question": "Vilken filmen är detta? &#129409; &#128081;",  
+                "answers": {
+                    "correct": "Lejonkungen",
+                    "wrong": ["Madagaskar", "Djungelboken"]
+                }
+            },
+            {
+                "question": "Vilken filmen är detta? &#128674; &#129482; &#128148;",  
+                "answers": {
+                    "correct": "Titanic",
+                    "wrong": ["Poseidon", "Pirates of the Caribbean"]
+                }
+            },
+            {
+                "question": "Vilken filmen är detta? &#128526; &#128299; &#128311; &#128138;",  
+                "answers": {
+                    "correct": "The Matrix",
+                    "wrong": ["Inception", "Terminator"]
+                }
+            },
+            {
+                "question": "Vilken filmen är detta? &#127993; &#128103; &#128293;",  
+                "answers": {
+                    "correct": "Hunger Games",
+                    "wrong": ["Brave", "Divergent"]
+                }
+            },
+            {
+                "question": "Vilken filmen är detta? &#129415; &#129333; &#127183;",  
+                "answers": {
+                    "correct": "The Dark Knight", // Fixed spelling of "Knight"
+                    "wrong": ["Spider-Man", "Batman Begins"]
+                }
+            }
+        ]
+    },
+    {
+        "category": "music",
+        "questions": [
+            {
+                "question": "vilken låt är detta? &#128103; &#128142; &#128141;",
+                "answers": {
+                    "correct": "Single Ladies (Beyoncé)",
+                    "wrong": ["Girls Just Wanna Have Fun (Cyndi Lauper)", "Diamonds (Rihanna)"]
+                }
+            },
+            {
+                "question": "vilken låt är detta? &#128156; &#9748;",
+                "answers": {
+                    "correct": "Purple Rain (Prince)",
+                    "wrong": ["November Rain (Guns N' Roses)", "Raindrops Keep Fallin' on My Head (B.J. Thomas)"]
+                }
+            },
+            {
+                "question": "vilken låt är detta? &#128064; &#128065; &#128005;",
+                "answers": {
+                    "correct": "Eye of the Tiger (Survivor)",
+                    "wrong": ["Roar (Katy Perry)", "Wild Ones (Flo Rida)"]
+                }
+            },
+            {
+                "question": "vilken låt är detta? &#128081 &#127926; &#128131;",
+                "answers": {
+                    "correct": "Dancing Queen (ABBA)",
+                    "wrong": ["Uptown Funk (Mark Ronson ft. Bruno Mars)", "Billie Jean (Michael Jackson)"]
+                }
+            },
+            {
+                "question": "Vilken låt är detta? &#127929;&#127925;&#128293;&#127936;",
+                "answers": {
+                    "correct": "Great Balls of Fire (Jerry Lee Lewis)",
+                    "wrong": ["Ring of Fire (Johnny Cash)", "Light My Fire (The Doors)"]
+                }
+            }
+
+        ]
+    }
+];
+
+let userAnswers = []
+let correctAnswers = []
+let askedQuestions = []
+
+function fillHome(){
+    document.body.innerHTML = ""
+    quizes.forEach(quiz => {
+        const categoryContainer = document.createElement("div") 
+        const title = document.createElement("h2")
+        title.textContent = quiz.category
+        categoryContainer.appendChild(title)
+        //console.log(quiz.category)
+
+        
+        title.addEventListener("click", function () {  
+            correctAnswers = []
+            quiz.questions.forEach(question => {
+                correctAnswers.push(question.answers.correct)
+                //console.log(correctAnswers)
+            }) 
+            printQuiz(quiz, 0)
+        })
+        document.body.appendChild(categoryContainer)
+    })
+
+}
+function displayResults() {
+    document.body.innerHTML = ""
+    const resultContainer = document.createElement("div")
+    const title = document.createElement("h2")
+    title.textContent = "Ditt resultat: "
+    resultContainer.appendChild(title)
+
+    let score = 0;
+    for (let i = 0; i < correctAnswers.length; i++) {
+        const resultSection = document.createElement("section")
+        const askedQuestion = document.createElement("p")
+        askedQuestion.innerHTML = askedQuestions[i]
+
+        const answer = document.createElement("p")
+        answer.textContent = "Du svarade: " + userAnswers[i]
+        const correctAnswer = document.createElement("p")
+        correctAnswer.textContent = "Rätt svar är: " + correctAnswers[i]
+
+        if (userAnswers[i] === correctAnswers[i]) {
+            score++;
+        }
+
+
+        resultSection.appendChild(askedQuestion)
+        resultSection.appendChild(answer)
+        resultSection.appendChild(correctAnswer)
+        resultContainer.appendChild(resultSection)
+    }
+     
+    
+    const result = document.createElement("p")
+    result.textContent = score + " av " + (correctAnswers.length)
+    resultContainer.appendChild(result)
+    const goHomeButton = document.createElement("button")
+    goHomeButton.textContent = "Till startsidan"
+    //console.log(askedQuestions)
+
+    goHomeButton.addEventListener("click", function () {
+        userAnswers = []
+        correctAnswers = []
+        askedQuestions = []
+        fillHome()
+    })
+  
+    resultContainer.appendChild(goHomeButton)
+    document.body.appendChild(resultContainer)
+  
+}
+function printQuiz(quiz, index) {
+    document.body.innerHTML = ""
+    const currentQuestion = quiz.questions[index]
+    askedQuestions.push(currentQuestion.question)
+    const container = document.createElement("div") 
+    const quizQuestion = document.createElement ("p")
+    quizQuestion.innerHTML = currentQuestion.question
+    const alternativeContainer = document.createElement("form")
+    const answers = [currentQuestion.answers.correct]
+    currentQuestion.answers.wrong.forEach(answer => {
+        answers.push(answer)
+    })
+    
+    answers.sort(() => Math.random() - 0.5)
+
+    answers.forEach(answer => {
+        const alternativ = document.createElement("input")
+        const radioLabel = document.createElement("label")
+        radioLabel.textContent = answer
+       
+        alternativ.type = "radio"
+        alternativ.label = answer
+        alternativ.name = currentQuestion.question
+        alternativ.value = answer
+        alternativ.id = answer
+        radioLabel.setAttribute("for", answer)
+        
+        alternativeContainer.appendChild(alternativ)  
+        alternativeContainer.appendChild(radioLabel)
+        
+    })
+    
+    const nextButton = document.createElement("input")
+    nextButton.type = "submit"
+    nextButton.value = "Nästa fråga" 
+
+    nextButton.addEventListener("click", function () {
+        const radioName = currentQuestion.question
+        const selectedRadio = document.querySelector(`input[name="${radioName}"]:checked`)
+
+        if (selectedRadio) {
+
+            userAnswers.push(selectedRadio.value)
+            //console.log(userAnswers)
+            if (index < quiz.questions.length -1) {
+                //console.log("test" + quiz.questions.length)
+                printQuiz(quiz, index + 1)
+            } else {
+                displayResults()
+            }
+            
+        } else {
+            alert("Du måste välja ett alternativ!")
+        }
+
+
+
+        
+    })
+
+    container.appendChild(quizQuestion)
+    container.appendChild(alternativeContainer)
+    container.appendChild(nextButton)
+
+
+
+    document.body.appendChild(container) 
+}
+fillHome() 
